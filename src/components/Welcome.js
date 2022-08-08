d77b3a99d6c316655a988fd2ffe1123fb3f0a1bb
Yaser Odat <yaserodat@outlook.com>
2022-08-08 21:13:51 +0300

    added auth0

---
+++ src/components/Welcome.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { withAuth0 } from '@auth0/auth0-react';
+
+class Welcome extends React.Component {
+    render() {
+
+        const { isAuthenticated } = this.props.auth0;
+        return (
+            !isAuthenticated &&( <> 
+                <h3>Welcome to Your Can of Books!</h3>
+                <p>Plase login your account:</p>
+            </>)
+        );
+    }
+}
+
+export default withAuth0(Welcome);
